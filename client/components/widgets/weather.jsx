485f5b320535595f84a0db7ccf8f7dcc59605e5a
hayduke19us <hayduke19us@gmail.com>
2015-09-21 16:00:03 -0700

    add font-awsome with react-fontawesome for easy access to icons

--- client/components/widgets/weather.jsx
+++ client/components/widgets/weather.jsx
@@ -1,10 +1,15 @@
-var React = require('react')
+var React = require('react');
+var FontA_ = require('react-fontawesome');
 
 module.exports = React.createClass({
   render: function (weather) {
     return (
       <div className='widget weather'>
         <h1>Weather</h1>
+        <div>
+          <FontA_ className='' name='rocket' size='2px' 
+          style={{ color: 'white' }} />
+        </div>
       </div>
     );
   }
