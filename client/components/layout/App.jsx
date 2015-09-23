052f60e438c0c2e9eebb234efe0f12b2c7ea7215
hayduke19us <hayduke19us@gmail.com>
2015-09-23 11:00:42 -0700

    add a inset boxshadow instead of padding and let the objets meld

--- client/components/layout/App.jsx
+++ client/components/layout/App.jsx
@@ -1,13 +1,16 @@
 var React = require('react');
+var FontA_ = require('react-fontawesome');
 
 module.exports = React.createClass({
   render: function () {
     return (
       <div className='main-content'>
-        <div id='weather'></div>
-        <div id='thermo'></div>
-        <div id='garden'></div>
-        <div id='internet'></div>
+        <div className='scroll-content'>
+          <div id='weather'></div>
+          <div id='thermo'></div>
+          <div id='garden'></div>
+          <div id='internet'></div>
+        </div>
         <div id='lights'></div>
       </div>
     );
